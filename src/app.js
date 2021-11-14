b5fc4a7dd732dd55d500e6d59a6a9064dcf6c473
insa-reblin <insa.reblin@hotmail.com>
2021-11-14 20:53:15 +0000

    added date/time

--- src/app.js
+++ src/app.js
@@ -1,16 +1,34 @@
-funtion displayTempreture(reponse) {
-    console.log(reponse,data.main.temp);
-    let tempretureElement = document.querySelector("#tempreture");
-    let cityElement = document.querySelector("city");
-    let descrpition = document.querySelector("descrpition");
-    tempretureElement.innerHTML = Math.round(reponse.data.main.temp);
-    cityElement.innerHTML = reponmse.data.name;
-    descrpition.innerHTML = response.data.weather[0].descrpition;
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = timestamp.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        mintues = `0${minutes}`;
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
 
 }
 
 
+function displayTempreture(response) {
+    let tempretureElement = document.querySelector("#tempreture");
+    let cityElement = document.querySelector("city");
+    let description = document.querySelector("description");
+    let dateElement = document.querySelector("date");
+    tempretureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    description.innerHTML = response.data.weather[0].description;
+    dateElement.innerHTML = formatDate(response,data.dt * 1000);
+}
+
+
 let apiKey = "edc18835dd6c780a8d86d394c2f2a19e";
-let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+let city = "London"
+let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTempreture);
