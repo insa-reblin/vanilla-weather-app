4adebf8af1a73cd9129a6b42a60bfb47c60a3a10
insa-reblin <insa.reblin@hotmail.com>
2021-11-21 12:34:59 +0000

    apped icon

--- src/app.js
+++ src/app.js
@@ -20,15 +20,18 @@ function displayTempreture(response) {
     let cityElement = document.querySelector("city");
     let description = document.querySelector("description");
     let dateElement = document.querySelector("date");
+    let iconElement = document.querySelector("icon");
+
     tempretureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     description.innerHTML = response.data.weather[0].description;
-    dateElement.innerHTML = formatDate(response,data.dt * 1000);
+    dateElement.innerHTML = formatDate(response,data.date * 1000);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
 
 let apiKey = "edc18835dd6c780a8d86d394c2f2a19e";
-let city = "London"
+let city = "New York"
 let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTempreture);
