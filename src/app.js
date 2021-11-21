dad9aeb051663961b9d70ff2c4b38fae65023787
insa-reblin <insa.reblin@hotmail.com>
2021-11-21 13:33:23 +0000

    added search form

--- src/app.js
+++ src/app.js
@@ -25,13 +25,27 @@ function displayTempreture(response) {
     tempretureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     description.innerHTML = response.data.weather[0].description;
-    dateElement.innerHTML = formatDate(response,data.date * 1000);
+    dateElement.innerHTML = formatDate(response.data.date * 1000);
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
-
+function search (city) {
 let apiKey = "edc18835dd6c780a8d86d394c2f2a19e";
-let city = "New York"
 let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTempreture);
+
+}
+
+
+function handelSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("city-input");
+    search(cityInputElement.value);
+    
+}
+
+search("New York")
+
+
+let form = document.querySelector("search-form");
+form.addEventListener("submit", handelSubmit);
