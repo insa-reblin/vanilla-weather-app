74efd7ef9d5041ec021b8f26f1153336c403c6d8
insa-reblin <insa.reblin@hotmail.com>
2021-12-12 13:37:38 +0000

    added API for forecast

--- src/app.js
+++ src/app.js
@@ -37,10 +37,10 @@ function formatDat(timestamp) {
   return days[day];
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
-  let forecastHTML = ` <div class ="row">`;
+  let forecastHTML = `<div class ="row">`;
   let days = ["Thurs", "Fri", "Sat", "Sun"];
   days.forEach(function (day) {
     forecastHTML =
@@ -68,6 +68,13 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "edc18835dd6c780a8d86d394c2f2a19e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude={part}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTempreture(response) {
   let tempretureElement = document.querySelector("#tempreture");
   let cityElement = document.querySelector("#city");
@@ -85,6 +92,8 @@ function displayTempreture(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -124,4 +133,3 @@ let celsuisLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("clik", displayCelsiusTemp);
 
 search("Singapore");
-displayForecast();
