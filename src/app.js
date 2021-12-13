bb2602ad8d40447bb5af26772370062539ac047c
insa-reblin <insa.reblin@hotmail.com>
2021-12-13 11:04:03 +0000

    updated js

--- src/app.js
+++ src/app.js
@@ -110,7 +110,7 @@ function displayTempreture(response) {
 
 function search(city) {
   let apiKey = "edc18835dd6c780a8d86d394c2f2a19e";
-  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTempreture);
 }
 
